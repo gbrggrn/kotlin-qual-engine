7e4f0d18327f1c163c0f9ab83d16d0e70a084318
gbrggrn <gustberg107@student.kau.se>
2026-02-04 09:33:18 +0100

    Schema updated to link docs + paragraphs + sentences

--- src/main/kotlin/com/qualengine/data/db/model/Schema.kt
+++ src/main/kotlin/com/qualengine/data/db/model/Schema.kt
@@ -4,45 +4,35 @@ import org.jetbrains.exposed.sql.Table
 
 object Documents : Table() {
     val id = varchar("id", 50)
-
     val content = text("content")
-
     val origin = varchar("source", 255)
-
     val timestamp = long("timestamp")
 
-    // Primary key id
     override val primaryKey = PrimaryKey(id)
 }
 
 object Sentences : Table() {
     val id = varchar("id", 50)
-
-    // Foreign key Document.id
     val docId = reference("doc_id", Documents.id)
-
     val content = text("content")
-
     val index = integer("index")
-
-    // Blobs later??
     val vector = text("vector").nullable()
+
+    val paragraphId = reference("paragraph_id", Paragraphs.id).nullable()
+    val status = varchar("status", 20).default("CLEAN")
 
     override val primaryKey = PrimaryKey(id)
 }
 
 object Paragraphs : Table() {
     val id = varchar("id", 50)
-
-    // Foreign key Document.id
     val docId = reference("doc_id", Documents.id)
-
     val content = text("content")
-
     val index = integer("index")
-
-    // Blobs later??
     val vector = text("vector").nullable()
+
+    val status = varchar("status", 20).default("CLEAN")
+    val sanityScore = double("sanityScore").default(1.0)
 
     override val primaryKey = PrimaryKey(id)
 }
