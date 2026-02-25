7a862535571f49cf382897bf9934e164e9bba894
gbrggrn <gustberg107@student.kau.se>
2026-02-25 09:49:29 +0100

    Documentation polish

--- src/main/kotlin/com/qualengine/core/AnalysisContext.kt
+++ src/main/kotlin/com/qualengine/core/AnalysisContext.kt
@@ -3,14 +3,14 @@ package com.qualengine.core
 import com.qualengine.data.model.AppState
 
 object AnalysisContext {
-    // === Stores AppState ===
+    // === Stores AppState
     private var _state = AppState()
 
-    // === Retrieve AppState ===
+    // === Retrieve AppState
     val state: AppState
         get() = _state
 
-    // === Update AppState ===
+    // === Update AppState
     fun update(newState: AppState) {
         _state = newState
     }
