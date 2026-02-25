a7d24454a6c06bfee13bb97662ed9a51cf5f177c
gbrggrn <gustberg107@student.kau.se>
2026-02-25 09:56:37 +0100

    Documentation polish

--- src/main/kotlin/com/qualengine/data/io/IOParser.kt
+++ src/main/kotlin/com/qualengine/data/io/IOParser.kt
@@ -4,7 +4,7 @@ import com.qualengine.data.model.TextBlock
 import java.io.File
 
 interface IOParser {
-    // --- Parses file into lazy sequence of TextBlocks
-    // --- Sequence: to only hold ONE (1) block in memory at a time!
+    // === Parses file into lazy sequence of TextBlocks
+    // === Sequence: to only hold ONE (1) block in memory at a time!
     fun parse(file: File): Sequence<TextBlock>
 }
